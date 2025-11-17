a8e23bc95e439cf01fd3642138f31969cc7625f5
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-11-17 20:38:08 -0300

    feat: add SecurityConfig

---
+++ springframework/src/main/java/br/com/fatec/login/configuration/SecurityConfig.java
@@ -0,0 +1,56 @@
+package br.com.fatec.login.configuration;
+
+import br.com.fatec.login.security.JWTSecurity;
+import br.com.fatec.login.security.JwtAuthFilterSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            HttpSecurity httpSecurity,
+            PasswordEncoder passwordEncoder,
+            UserDetailsService userDetailsService) throws Exception {
+        AuthenticationManagerBuilder builder = httpSecurity
+                .getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(userDetailsService).
+                passwordEncoder(passwordEncoder);
+        return builder.build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            JWTSecurity jwtSecurity,
+            HttpSecurity httpSecurity,
+            UserDetailsService userDetails) throws Exception {
+        JwtAuthFilterSecurity jwtFilter = new JwtAuthFilterSecurity(jwtSecurity, userDetails);
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/users/save/**",
+                                "/auth/login/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+}
